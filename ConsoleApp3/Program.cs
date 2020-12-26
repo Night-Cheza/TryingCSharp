5a1bf11d1b06b1599589a232255d0b4c85163431
Leila Learning <4everwith@gmail.com>
2020-12-26 12:41:01 -0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AnimalClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewAnimal a = new NewAnimal();
+            a.name = "lion";
+            a.description = "a very big and hungry cat";
+            a.location = "lives in Africa";
+            a.height = 1.0;//meters
+            a.weight = 200;//kg
+
+                       
+        }
+    }
+
+        public class NewAnimal
+    {
+        public string name;
+        public string description;
+        public string location;
+        public double height;
+        public int weight;
+        
+
+        public void CanRun()
+        {
+            Console.WriteLine("This animal can run");
+        }
+        public void NeedToEat()
+        {
+            Console.WriteLine("This animal needs to eat");
+        }
+        public void NeedToSleep()
+        {
+            Console.WriteLine("This animal needs to sleep");
+        }
+
+        
+
+    }
+
+}
